5e541a75c7876860da9fb1e9bbf3fda882457f09
Andrew Hammett <hammett@amh.io>
2017-12-13 17:15:05 -0800

    duplication makes me sad

--- src/update.js
+++ src/update.js
@@ -3,8 +3,29 @@
 const AWS = require('aws-sdk'); // eslint-disable-line import/no-extraneous-dependencies
 
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
+const allowCidr = process.env.ALLOW_CIDR || 'x.x.x.x'
 
 module.exports.update = (event, context, callback) => {
+  var found = false;
+
+  allowCidr.split(' ').forEach(function(allow_mask) {
+    if(event['requestContext']['identity']['sourceIp'].includes(allow_mask)) {
+      found = true
+    }
+  });
+
+  if(!found) {
+    console.error('Requestor not in allow list')
+
+    callback(null, {
+      statusCode: 403,
+      headers: { 'Content-Type': 'text/plain' },
+      body: '¯\\_(ツ)_/¯',
+    });
+
+    return;
+  }
+
   const timestamp = new Date().getTime();
   const data = JSON.parse(event.body);
 
